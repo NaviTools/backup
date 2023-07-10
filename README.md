13c14746093c31983a03ffb17da9ae22c7beab14
Javier Bullrich <javier@bullrich.dev>
2023-07-10 11:07:05 +0200

    main commit of backup tool
    This will generate a backup every night at 5am

---
+++ README.md
@@ -0,0 +1,15 @@
+# Backup tool
+
+Automatically generates backups to an [S3 Bucket](https://s3.console.aws.amazon.com/s3/)
+
+## Setup
+
+Requires the following variables:
+```shell
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_S3_BUCKET=
+AWS_S3_REGION=
+BACKUP_DATABASE_URL=
+ROLLBAR_ACCESS_TOKEN=
+```
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { CronJob } from "cron";
+
+import { backup } from "./backup";
+import { env } from "./env";
+import Rollbar, { LogArgument } from "rollbar";
+
+var rollbar = new Rollbar({
+    accessToken: env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+});
+
+const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
+    try {
+        await backup();
+        rollbar.log("Job completed succesfully");
+    } catch (error) {
+        console.error("Error while running backup: ", error);
+        rollbar.error(error as LogArgument);
+    }
+});
+
+job.start();
+
+console.log("Backup cron scheduled...");
