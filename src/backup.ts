45afba15393648f298df15ffb7da036452cac520
Javier Bullrich <javier@bullrich.dev>
2024-02-27 10:56:41 +0100

    Fixed backup file not being generated
    Updated the code to be on pair with https://github.com/railwayapp-templates/postgres-s3-backups/commit/fca2eaa0b2e581d871ab271c9b7c78fb8a1af49f where they change the logic

--- src/backup.ts
+++ src/backup.ts
@@ -1,6 +1,8 @@
 import { PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
-import { exec } from "child_process";
-import { createReadStream, unlink } from "fs";
+import { exec, execSync } from "child_process";
+import { createReadStream, unlink, statSync } from "fs";
+import { filesize } from "filesize";
+import {basename} from "path";
 
 import { env } from "./env";
 
@@ -36,11 +38,31 @@ const dumpToFile = async (path: string) => {
 
     await new Promise((resolve, reject) => {
         exec(
-            `pg_dump ${env.BACKUP_DATABASE_URL} -F t | gzip > ${path}`,
+            `pg_dump --dbname=${env.BACKUP_DATABASE_URL} --format=tar | gzip > ${path}`,
             (error, stdout, stderr) => {
                 if (error) {
                     reject({ error: JSON.stringify(error), stderr });
                     return;
+                }
+
+                // check if archive is valid and contains data
+                const isValidArchive = (execSync(`gzip -cd ${path} | head -c1`).length == 1) ? true : false;
+                if (isValidArchive == false) {
+                    reject({ error: "Backup archive file is invalid or empty; check for errors above" });
+                    return;
+                }
+
+                // not all text in stderr will be a critical error, print the error / warning
+                if (stderr != "") {
+                    console.log({ stderr: stderr.trimEnd() });
+                }
+
+                console.log("Backup archive file is valid");
+                console.log("Backup filesize:", filesize(statSync(path).size));
+
+                // if stderr contains text, let the user know that it was potently just a warning message
+                if (stderr != "") {
+                    console.log(`Potential warnings detected; Please ensure the backup file "${basename(path)}" contains all needed data`);
                 }
 
                 resolve(undefined);
